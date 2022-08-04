34ae339aae3cec30cd8e158e48cbdd9834394794
yyuff <ezyyuff@163.com>
2022-08-04 16:27:09 +0800

    Fixed Flik library

---
+++ lab4/flik/Test.java
@@ -0,0 +1,10 @@
+package flik;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer i = 129;
+        Integer j = 129;
+        System.out.println(i == j);
+        System.out.println(i.equals(j));
+    }
+}
